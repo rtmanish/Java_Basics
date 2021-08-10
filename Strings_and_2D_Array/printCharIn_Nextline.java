2f97c97efafe4ed80f0114e9ac30ccc12113e97d
Manish <rohitmanish002@gmail.com>
2021-08-10 11:52:33 +0530

    Java Codes

---
+++ Strings_and_2D_Array/printCharIn_Nextline.java
@@ -0,0 +1,17 @@
+package Strings_and_2D_Array;
+
+public class printCharIn_Nextline {
+
+	public static void printchar(String str) {
+		for(int i=0;i<=str.length() - 1;i++) {
+			System.out.println(str.charAt(i));
+		}
+	}
+	public static void main(String[] args) {
+
+		String s="Rohit";
+		printchar(s);
+
+	}
+
+}
